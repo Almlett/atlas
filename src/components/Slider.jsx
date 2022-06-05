4a6870b84c1326ee64f60af14b6154be92295433
root <root@LAPTOP-DCJJ3HIT.localdomain>
2022-06-04 21:47:17 -0700

    first commit

---
+++ src/components/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Video from './Video.jsx';
+
+const Slider = () => {
+    return ( 
+        <div>
+            <Video />
+        </div>
+     );
+}
+ 
+export default Slider;
